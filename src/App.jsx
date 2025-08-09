b6725ee8815ef994a90c85ed5b24ac4e049ea533
finaurthub <akshanshchauhan47@gmail.com>
2025-08-09 11:54:22 +0530

    v3.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import './App.scss'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import TopSlidingText from './Components/TopSlidingText'
-import TopStrip from './Components/TopStrip'
-import Navigation from './Components/Navigation'
-import Footer from './Components/Footer'
-import About from './Pages/About'
-import Courses from './Pages/Courses'
-import Services from './Pages/Services'
-import Blogs from './Pages/Blogs'
-import Contact from './Pages/Contact'
+import "./App.scss";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import TopSlidingText from "./Components/TopSlidingText";
+import TopStrip from "./Components/TopStrip";
+import Navigation from "./Components/Navigation";
+import Footer from "./Components/Footer";
+import About from "./Pages/About";
+import Courses from "./Pages/Courses";
+import Services from "./Pages/Services";
+import Blogs from "./Pages/Blogs";
+import Contact from "./Pages/Contact";
+import { useEffect } from "react";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
-    <div className='app'>
+    <div className="app">
       <TopSlidingText />
       <TopStrip />
       <Navigation />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/courses' element={<Courses />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/blogs' element={<Blogs />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-top">
         <div className="footer-column about">
-          <a href="https://pharmaconnections.in" target="_blank" rel="noreferrer">
+          <a
+            href="https://pharmaconnections.in"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img
               src="https://pharmaconnections.in/wp-content/uploads/2021/11/WhatsApp-Image-2024-08-31-at-04.34.55-1-1-removebg-preview.png"
               alt="Pharma Connections"
@@ -15,13 +21,21 @@ const Footer = () => {
         <div className="footer-column links">
           <h2>Quick Links</h2>
           <ul>
-            <li><a href="https://pharmaconnections.in/">Home</a></li>
-            <li><a href="https://pharmaconnections.in/about-us/">About Us</a></li>
-            <li><a href="https://pharmaconnections.in/ourservices/">Our Services</a></li>
-            <li><a href="https://pharmaconnections.in/accreditations/">Accreditations</a></li>
-            <li><a href="https://pharmaconnections.in/blog/">Blogs & Articles</a></li>
-            <li><a href="https://pharmaconnections.in/contact-us/">Contact Us</a></li>
-            <li><a href="https://pharmaconnections.in/sitemap/">Sitemap</a></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/services">Our Services</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blogs & Articles</Link>
+            </li>
+            <li>
+              <Link to="/contact-us">Contact Us</Link>
+            </li>
           </ul>
         </div>
 
@@ -32,8 +46,16 @@ const Footer = () => {
           <p>50/51-4, NS Palya, BTM Layout, 2nd Stage,</p>
           <p>Bengaluru, Karnataka 560076</p>
           <div className="contact-info">
-            <p><strong>Phone:</strong> <a href="tel:+918660877518">+91 86608-77518</a></p>
-            <p><strong>Mail Us:</strong> <a href="mailto:info@pharmaconnections.in">info@pharmaconnections.in</a></p>
+            <p>
+              <strong>Phone:</strong>{" "}
+              <a href="tel:+918660877518">+91 86608-77518</a>
+            </p>
+            <p>
+              <strong>Mail Us:</strong>{" "}
+              <a href="mailto:info@pharmaconnections.in">
+                info@pharmaconnections.in
+              </a>
+            </p>
           </div>
         </div>
       </div>
@@ -41,9 +63,21 @@ const Footer = () => {
       <div className="footer-bottom">
         <p>Copyright © 2025 Pharma Connections. All Rights Reserved.</p>
         <div className="footer-bottom-links">
-          <a href="https://pharmaconnections.in/terms-of-use/" target="_blank" rel="noreferrer">Terms Of Use</a>
+          <a
+            href="https://pharmaconnections.in/terms-of-use/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Terms Of Use
+          </a>
           <span>|</span>
-          <a href="https://pharmaconnections.in/privacy-policy/" target="_blank" rel="noreferrer">Privacy Policy</a>
+          <a
+            href="https://pharmaconnections.in/privacy-policy/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Privacy Policy
+          </a>
         </div>
       </div>
     </footer>
